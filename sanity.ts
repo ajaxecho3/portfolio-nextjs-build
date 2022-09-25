fce6ffc0a3813f4070cae24739ff8c9cdde869b0
bernad <ajaxecho3@gmail.com>
2022-09-25 19:45:31 +0800

    manor change

--- sanity.ts
+++ sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from '@sanity/image-url'
-export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+export const config:any = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
