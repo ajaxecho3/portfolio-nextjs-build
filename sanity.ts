651937eda8025fcaf299ba794d814e9717ca411d
bernad <ajaxecho3@gmail.com>
2022-09-30 18:09:43 +0800

    minor update

--- sanity.ts
+++ sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from '@sanity/image-url'
 export const config:any = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: "2021-03-25",
   useCdn: process.env.NODE_ENV === "production",
 };
